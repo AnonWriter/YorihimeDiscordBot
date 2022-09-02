5978c3897891847688e8dfcc19a2eb93fdc6986c
AnonWriter <98379144+AnonWriter@users.noreply.github.com>
2022-09-02 15:04:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+const { SSRCMap } = require('@discordjs/voice');
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
 const fs = require("fs");
+const { send } = require('process');
 
 dotenv.config();
 
@@ -14,8 +16,8 @@ const Yori = new Discord.Client(
     }
 );
 
-const token = process.env.Yorihime;
-const prefix = "!yori ";
+const token = process.env.Pornografo;
+const prefix = "!pn ";
 
 Yori.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -30,20 +32,48 @@ for (const file of commandFiles)
 Yori.once('ready', () =>
 {
     console.log(`Conección existosa: ${Yori.user.tag}`);
+    Yori.user.setActivity("porno", );
+    Yori.user.setPresence(
+        {
+            status: 'idle',
+        }
+    )
 });
 
 Yori.on("messageCreate", message =>
 {
-    if (message.author.tag !== Yori.user.tag) console.log(`Leyendo mensaje... ${message.content}`);
+    /* if (message.author.tag !== Yori.user.tag) console.log(`Leyendo mensaje... ${message.content}`); */
     if (message.author.tag === Yori.user.tag) return;
     if(message.author.bot)
     {
         console.log("Mensaje enviado por un bot, ignorando.");
         return;
     }
+
+    switch (message.content.toLowerCase())
+    {
+        case 'marlos': case 'barrera': case 'malros':
+            message.channel.send('es joto!');
+            break;
+        case 'gama': case 'gmaa': case 'gaam':
+            message.channel.send('el peinado de culo abierto');
+            break;
+        case 'ronni': case 'roni': case 'ranas':
+            message.channel.send('G1');
+            break;
+        case 'carlos': case 'cralos': case 'carlos': case 'escritor':
+            message.channel.send('el escirtor');
+            break;
+        case 'maria': case 'mariana':
+            message.channel.send('le gusta el gama');
+            break;
+        case 'eri': case 'eriberto': case 'eir':
+            message.channel.send('se fue al gabacho');
+            break;
+    }
+
     if(!message.content.startsWith(prefix))
     {
-        console.log("El mensaje no contiene el prefijo, ignorando.");
         return;
     }
 
@@ -65,10 +95,9 @@ Yori.on("messageCreate", message =>
             Yori.commands.get('ponte').execute(message, Yori, prefix, args);
             break;
         case 'img': case 'imagen': case 'image':
-            Yori.commands.get('img').execute(message, args)
+            Yori.commands.get('img').execute(message, args);
             break;
     }
-    console.log(`Comando [${cmd}] ejecutado por ${message.author.tag}`);
 });
 
 Yori.login(token);
