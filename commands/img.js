1cc1be73c00224f300be90e18532dad69bb1b60a
AnonWriter <98379144+AnonWriter@users.noreply.github.com>
2022-09-02 15:06:33 -0500

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 var Scraper = require("images-scraper");
 
 const google = new  Scraper({
@@ -11,10 +12,26 @@ module.exports = {
     description: 'Agarra una imágen de google de lo que pidas.',
     async execute(message, args)
     {
-        const imageQuery = args.join(' ');
-        if (!imageQuery) return message.channel.send('Especifica qué vas a buscar.');
+        var randomNumber= Math.floor(Math.random() * 9);
 
-        const imageResults = await google.scrape(imageQuery, 1);
-        message.channel.send(imageResults[0].url);
+        const imageQuery = args.join(' ');
+        var noNumber = imageQuery.length;
+        if (!imageQuery) return message.channel.send('Especifica qué vas a buscar.');
+        if (!isNaN(imageQuery[imageQuery.length-1]))
+        {
+            randomNumber = imageQuery[imageQuery.length-1];
+            noNumber-=1;
+        }
+
+        const imageResults = await google.scrape(imageQuery.slice(0, noNumber), 10);
+
+        const imgEmbed = new MessageEmbed()
+            .setDescription(`***${imageQuery.slice(0, noNumber)}***\n ***[Imagen](${imageResults[randomNumber].url})*** *\`${randomNumber}\` de \`9\`*`)
+            .setColor(message.author.hexAccentColor)
+            .setImage(imageResults[randomNumber].url);
+
+        message.channel.send({
+            embeds: [imgEmbed]
+        });
     }
 }
