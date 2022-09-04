903900ac71a7d0f370c3c3b2816173bfd18e65e6
AnonWriter <98379144+AnonWriter@users.noreply.github.com>
2022-09-04 01:25:48 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,15 +4,60 @@ const fs = require('fs')
 module.exports = {
     name: 'help',
     description: 'Recibe un mensaje de ayuda de Yorihime',
-    execute(message, Yori)
+    execute(message, Yori, prefix, cmd, args)
     {
+        var file = 'helpv3.txt';
+
+        /*var subarg = message.content.slice(prefix.length + cmd.length);*/
+
+        switch(args[0])
+        {
+            case 'github': case 'git':
+                file = 'github.txt';
+                break;
+            case '?': case 'help': case 'ayuda': case 'info':
+                file = 'shelp.txt';
+                break;
+            case 'di':
+                file = 'dihelp.txt';
+                break;
+            case 'borra': case 'elimina':
+                file = 'borrahelp.txt';
+                break;
+            case 'img': case 'imagen': case 'image':
+                file = 'imghelp.txt';
+                break;
+            case 'ponte':
+                file = 'pontehelp.txt';
+                break;
+            case 'palabras':
+                file = 'palabrashelp.txt';
+                break;
+            case 'updates': case 'versions':case 'version':
+            case 'versiones':case 'actualizaciones': case 'update':
+                file = 'updhelp.txt';
+                break;
+            case 'ask': case 'pregunta': case '¿':
+                file = 'askhelp.txt';
+                break;
+            case 'ping':
+                file = 'pinghelp.txt';
+                break;
+            case 'bonk':
+                file = 'bonkhelp.txt';
+                break;
+            case 'nobonk':
+                file = 'nobonkhelp.txt';
+                break;
+        }
+
         const helpEmbed = new MessageEmbed()
             .setAuthor(`${Yori.user.tag}`, Yori.user.displayAvatarURL())
-            .setTitle(`Lista de comandos\npedidos por ${message.author.tag}:`)
+            .setTitle(`Ayuda:`)
             .setColor('#ccb4e8')
-            .setDescription(fs.readFileSync(`./help/helpv1.txt`, 'utf-8'));
+            .setDescription(fs.readFileSync(`./help/${file}`, 'utf-8'));
 
-        message.author.send({
+        message.channel.send({
             embeds: [helpEmbed]
         });
     }
