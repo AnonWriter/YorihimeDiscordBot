3280c939badafb40953945644b2b4fa5a44b3d78
AnonWriter <98379144+AnonWriter@users.noreply.github.com>
2022-09-04 00:29:09 -0500

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -12,26 +12,47 @@ module.exports = {
     description: 'Agarra una imágen de google de lo que pidas.',
     async execute(message, args)
     {
-        var randomNumber= Math.floor(Math.random() * 9);
+        try {
+            var randomNumber= Math.floor(Math.random() * 20);
 
-        const imageQuery = args.join(' ');
-        var noNumber = imageQuery.length;
-        if (!imageQuery) return message.channel.send('Especifica qué vas a buscar.');
-        if (!isNaN(imageQuery[imageQuery.length-1]))
-        {
-            randomNumber = imageQuery[imageQuery.length-1];
-            noNumber-=1;
+            const imageQuery = args.join(' ');
+            var noNumber = imageQuery.length;
+            if (!imageQuery) return message.channel.send('Especifica qué vas a buscar.');
+            if (!isNaN(imageQuery[imageQuery.length-1]))
+            {
+                var start = 0;
+                if (isNaN(imageQuery[imageQuery.length-2]))
+                {
+                    start = imageQuery.length-1;
+                    noNumber-=1;
+                } else
+                {
+                    start = imageQuery.length-2;
+                    noNumber-=2;
+                }
+                randomNumber = parseInt(imageQuery.slice(start));
+            }
+
+            const imageResults = await google.scrape(imageQuery.slice(0, noNumber), 21)
+            if (!imageResults) message.channel.send('No se pudo.');
+
+            const imgEmbed = new MessageEmbed()
+                .setDescription(`***${imageQuery.slice(0, noNumber)}***\n ***[Imagen](${imageResults[randomNumber].url})*** *\`${randomNumber}\` de \`20\`*`)
+                .setColor(message.author.hexAccentColor)
+                .setImage(imageResults[randomNumber].url);
+
+            message.channel.send({
+                embeds: [imgEmbed]
+            });
         }
+        catch(err){
+            const current = new Date();
 
-        const imageResults = await google.scrape(imageQuery.slice(0, noNumber), 10);
+            const date = current.getFullYear()+'/'+(current.getMonth()+1)+'/'+current.getDate();
+            const time = current.getHours()+':'+current.getMinutes()+':'+current.getSeconds();
 
-        const imgEmbed = new MessageEmbed()
-            .setDescription(`***${imageQuery.slice(0, noNumber)}***\n ***[Imagen](${imageResults[randomNumber].url})*** *\`${randomNumber}\` de \`9\`*`)
-            .setColor(message.author.hexAccentColor)
-            .setImage(imageResults[randomNumber].url);
-
-        message.channel.send({
-            embeds: [imgEmbed]
-        });
+            message.channel.send('*`Hubo un problema al cargar la imagen.`*');
+            console.error(`${date} ${time} | Intento fallido de enviar una imagen ${err}`);
+        }
     }
 }
